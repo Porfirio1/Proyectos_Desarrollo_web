a064c5734dd15054626b527cd275ac63f1e60767
Porfirio <porfis1720@gmail.com>
2025-07-17 20:05:22 -0600

    https://github.com/Porfirio1/Proyectos.git

--- Proyecto-2/assets/js/main.js
+++ Proyecto-2/assets/js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Proyecto 2 
 // Archivo con funciones JavaScript para el proyecto
 
@@ -19,4 +20,27 @@ window.addEventListener('DOMContentLoaded', () => { // Espera a que el DOM esté
         }
     })
 
+=======
+// Proyecto 2 
+// Archivo con funciones JavaScript para el proyecto
+
+window.addEventListener('DOMContentLoaded', () => { // Espera a que el DOM esté completamente cargado
+
+    // Selecciona los elementos del DOM
+    let input = document.querySelector('.main__input'); // Selecciona el input de búsqueda
+    let text = document.querySelector('.main__text'); // Selecciona el párrafo de texto
+
+    input.addEventListener('keydown', (e) => {
+
+        let search = input.value;
+
+        if(search.trim() != ""  && e.code == "Enter"){ // Verifica si el input no está vacío y si se presionó la tecla Enter
+            let regExp = new RegExp(search, "gi"); // Crea una expresión regular para buscar la palabra ingresada, ignorando mayúsculas y minúsculas
+
+            text.innerHTML = text.textContent.replace(regExp, "<span class='text__search'>$&</span>"); // Reemplaza las coincidencias con un span que resalta la palabra buscada
+
+        }
+    })
+
+>>>>>>> 532fc59 (https://github.com/Porfirio1/Proyectos.git)
 });
